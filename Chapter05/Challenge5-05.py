69d6e497606406f39f92ba6f88fa14d257fda8e2
itc-n25014 <n25014@std.it-college.ac.jp>
2025-06-06 11:18:47 +0900

    P87 第5章 チャレンジ問題 終了

---
+++ Chapter05/Challenge5-05.py
@@ -0,0 +1,6 @@
+songs = {
+    "SEREBRO": ["Отпусти меня", "Я тебя не отдам"],
+    "Amir Haddad": ["J'aicherché", "Que seront les hommes?"],
+    "Три дня дождя": ["Прощание", "Подозрительно"]
+}
+print(songs)
